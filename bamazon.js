df207504020855ff8988382d57737cd5c4beef46
Brady Withers <brady.withers@outlook.com>
2017-12-16 19:17:54 -0700

    working

--- bamazon.js
+++ bamazon.js
@@ -29,4 +29,89 @@ connection.connect(function(error) {
             console.log("Quantity: " + response[i].stock_quantity);
             console.log("----------");
         }
+
+        inquirer.prompt([{
+            name: "id",
+            type: "input",
+            message: "What would you like to buy? Enter ID: ",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }, {
+            name: "units",
+            type: "input",
+            message: "How many would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+
+        }]).then(function(answer) {
+
+            for (var i = 0; i < response.length; i++) {
+                if (response[i].item_id === parseInt(answer.id))
+                    stock(parseInt(answer.id), answer.units);
+            }
+        });
     });
+
+
+function stock(itemID, units) {
+    connection.query("SELECT * FROM products WHERE ?", {
+        item_id: itemID
+    }, function(error, response) {
+        if (error) throw error;
+
+        if (response[0].stock_quantity <= 0) {
+            console.log("Insufficient quantity!");
+           
+        } else
+            updateQuantity(itemID, units);
+    });
+}
+
+function cost(itemID, units) {
+    connection.query("SELECT * FROM products WHERE ?", {
+        item_id: itemID
+    }, function(error, response) {
+        if (error) throw error;
+
+        var totalCost = response[0].price * units;
+        console.log("Total cost is $ " + totalCost);
+
+        
+    });
+}
+
+function updateQuantity(itemID, units) {
+    connection.query("SELECT * FROM products WHERE ?", {
+        item_id: itemID
+    }, function(error, response) {
+        if (error) throw error;
+
+        var newQuantity = response[0].stock_quantity - units;
+
+        if (newQuantity < 0)
+            newQuantity = 0;
+
+        connection.query("UPDATE products SET ? WHERE ?", [{
+            stock_quantity: newQuantity
+        }, {
+            item_id: itemID
+        }], function(error, response) {});
+
+        cost(itemID, units);
+    });
+}
+
+
+
+
+
+
+  
